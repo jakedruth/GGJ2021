457df6b5a563b3473d3618b7a8a6691c6cc05328
Jakedruth@gmail.com <Jakedruth@gmail.com>
2021-01-27 18:02:48 -0500

    added helper functions I use regularly and implemented a game manager

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public bool isPaused { get; private set; }
+    public UnityEventBool onPausedChanged;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void TogglePause()
+    {
+        SetPause(!isPaused);
+    }
+
+    public void SetPause(bool value)
+    {
+        if (isPaused == value)
+            return;
+
+        onPausedChanged?.Invoke(value);
+
+        isPaused = value;
+    }
+
+    // TODO: move this to a new locations
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            QuitGame();
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
+
+[System.Serializable]
+public class UnityEventBool : UnityEvent<bool>
+{ }
