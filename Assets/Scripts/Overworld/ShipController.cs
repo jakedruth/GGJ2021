955c0ea18e9fe275811bea69996c64ce7f353b51
Jakedruth@gmail.com <Jakedruth@gmail.com>
2021-01-28 13:00:17 -0500

    Implemented Sailing boost from crew

--- Assets/Scripts/Overworld/ShipController.cs
+++ Assets/Scripts/Overworld/ShipController.cs
@@ -4,48 +4,71 @@ using UnityEngine;
 
 public class ShipController : MonoBehaviour
 {
-    [Header("Movement Variables")]
-    public float maxSpeed;
+    [Header("Ship Health")]
+    public float maxHP;
+    public float hp;
+
+    [Header("Crew Variables")]
+    [PlusMinus(0, 100)]
+    public int sailCrew;
+    [PlusMinus(0, 100)]
+    public int cannonCrew;
+    [PlusMinus(0, 100)]
+    public int repairCrew;
+
+    [Header("Sail/ Movement Variables")]
+    public float baseSpeed;
+    public float crewSpeedBonus;
     private float _speed;
     public float turnSpeed;
     private float _heading;
     public float acceleration;
 
-
     [Header("Cannon Ball Variables")] 
     public string cannonBallPrefabName;
-    public float reloadCannonRate;
+    public float reloadBaseRate;
+    public float crewReloadBonus;
     public Transform leftCannonFirePoint;
     public Transform rightCannonFirePoint;
     private float _leftCannonCoolDown;
     private float _rightCannonCoolDown;
 
+    void Start()
+    {
+        SetHPToMax();
+    }
+
     void Update()
     {
         // Handle Left Cannon
         if (_leftCannonCoolDown < 1)
-            _leftCannonCoolDown += reloadCannonRate * Time.deltaTime;
+            _leftCannonCoolDown += reloadBaseRate * Time.deltaTime;
         else
             _leftCannonCoolDown = 1;
 
         // Handle Right Cannon
         if (_rightCannonCoolDown < 1)
-            _rightCannonCoolDown += reloadCannonRate * Time.deltaTime;
+            _rightCannonCoolDown += reloadBaseRate * Time.deltaTime;
         else
             _rightCannonCoolDown = 1;
+    }
+
+    public void SetHPToMax()
+    {
+        hp = maxHP;
     }
 
     public void HandleMovementInput(Vector2 input)
     {
         bool hasInput = input.sqrMagnitude > 0;
+        float targetSpeed = hasInput ? baseSpeed + crewSpeedBonus * sailCrew : 0;
 
-        float targetSpeed = hasInput ? maxSpeed : 0;
         _speed = Mathf.MoveTowards(_speed, targetSpeed, acceleration * Time.deltaTime);
 
         if (hasInput)
         {
             float targetAngle = Vector2.SignedAngle(Vector2.right, input);
-            _heading = Mathf.MoveTowardsAngle(_heading, targetAngle, turnSpeed * _speed / maxSpeed * Time.deltaTime);
+            _heading = Mathf.MoveTowardsAngle(_heading, targetAngle, turnSpeed * _speed / baseSpeed * Time.deltaTime);
             transform.localRotation = Quaternion.AngleAxis(_heading, Vector3.forward);
         }
 
