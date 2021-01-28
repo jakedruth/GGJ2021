c1e20ebe7bc1e27475e566bea10d1d03d54fd4e4
Jakedruth@gmail.com <Jakedruth@gmail.com>
2021-01-28 00:32:47 -0500

    Implemented Cannonballs being fired

---
+++ Assets/Scripts/Overworld/Cannonball.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannonball : MonoBehaviour
+{
+    public float moveSpeed;
+    public float fireDistance;
+    public AnimationCurve scaleOverTime;
+    private float _totalLifeTime;
+    private float _lifeTime;
+    private Transform _owner;
+    private Vector3 _inheritVel;
+
+    void Start()
+    {
+        _totalLifeTime = fireDistance / moveSpeed;
+        transform.GetChild(0).right = Vector3.right;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _lifeTime += Time.deltaTime;
+        if (_lifeTime >= _totalLifeTime)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        transform.GetChild(0).localScale = Vector3.one * scaleOverTime.Evaluate(_lifeTime / _totalLifeTime);
+        transform.position += (transform.right * moveSpeed) * Time.deltaTime;
+    }
+
+    public void InitCannonBall(Transform owner, Vector3 inheritVel)
+    {
+        _owner = owner;
+        _inheritVel = inheritVel;
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform == _owner)
+            return;
+
+        Debug.Log($"Hit Ship: {collision.gameObject.name}");
+    }
+
+
+}
--- Assets/Scripts/Overworld/ShipController.cs
+++ Assets/Scripts/Overworld/ShipController.cs
@@ -4,11 +4,36 @@ using UnityEngine;
 
 public class ShipController : MonoBehaviour
 {
+    [Header("Movement Variables")]
     public float maxSpeed;
     private float _speed;
     public float turnSpeed;
     private float _heading;
     public float acceleration;
+
+
+    [Header("Cannon Ball Variables")] 
+    public string cannonBallPrefabName;
+    public float reloadCannonRate;
+    public Transform leftCannonFirePoint;
+    public Transform rightCannonFirePoint;
+    private float _leftCannonCoolDown;
+    private float _rightCannonCoolDown;
+
+    void Update()
+    {
+        // Handle Left Cannon
+        if (_leftCannonCoolDown < 1)
+            _leftCannonCoolDown += reloadCannonRate * Time.deltaTime;
+        else
+            _leftCannonCoolDown = 1;
+
+        // Handle Right Cannon
+        if (_rightCannonCoolDown < 1)
+            _rightCannonCoolDown += reloadCannonRate * Time.deltaTime;
+        else
+            _rightCannonCoolDown = 1;
+    }
 
     public void HandleMovementInput(Vector2 input)
     {
@@ -27,5 +52,30 @@ public class ShipController : MonoBehaviour
         Vector3 vel = transform.right * _speed;
 
         transform.position += vel * Time.deltaTime;
+    }
+
+    public void FireLeft()
+    {
+        if (_leftCannonCoolDown < 1)
+            return;
+
+        _leftCannonCoolDown = 0;
+        SpawnCannonballAtTransform(leftCannonFirePoint);
+    }
+
+    public void FireRight()
+    {
+        if (_rightCannonCoolDown < 1)
+            return;
+
+        _rightCannonCoolDown = 0;
+        SpawnCannonballAtTransform(rightCannonFirePoint);
+    }
+
+    private void SpawnCannonballAtTransform(Transform t)
+    {
+        Cannonball resource = Resources.Load<Cannonball>($"Prefabs/{cannonBallPrefabName}");
+        Cannonball cb = Instantiate(resource, t.position, t.rotation);
+        cb.InitCannonBall(transform, transform.right * _speed);
     }
 }
