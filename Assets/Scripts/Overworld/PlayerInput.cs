ddfdc14731ef9b46a6cb007431b050a7e6708fd4
Jakedruth@gmail.com <Jakedruth@gmail.com>
2021-01-28 16:17:38 -0500

    Changes to input

--- Assets/Scripts/Overworld/PlayerInput.cs
+++ Assets/Scripts/Overworld/PlayerInput.cs
@@ -17,17 +17,15 @@ public class PlayerInput : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 inputDirection = Vector2.zero;
         if (Input.GetMouseButton(0))
         {
             Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Vector2 pos = transform.position;
 
             Vector3 delta = mousePos - pos;
-            inputDirection = delta.normalized;
+            _shipController.HandleMovementInput(delta.normalized);
         }
 
-        _shipController.HandleMovementInput(inputDirection);
 
         if (Input.GetKeyDown(KeyCode.A))
         {
