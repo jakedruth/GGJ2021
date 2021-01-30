0a441658314eff8c365466db72a0ce6e1f7bb25d
Jakedruth@gmail.com <Jakedruth@gmail.com>
2021-01-30 13:51:31 -0500

    started work on an island class

---
+++ Assets/Scripts/Overworld/Island.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Island : MonoBehaviour
+{
+    public float difficultyLevel;
+
+    public void Start()
+    {
+
+    }
+
+
+}
